661961840e71e2e9b8862176305455905cbbb718
jonnew <jpnewman@mit.edu>
2023-01-13 18:00:09 -0500

    Initial commit

---
+++ Bonsai.OpenEphys/AcquisitionBoard.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Threading;
+
+using oni;
+
+namespace Bonsai.OpenEphys
+{
+    public class AcquisitionBoard : Bonsai.Source<IList<oni.Frame>>
+    {
+
+        [Range(0,100)]
+        public int USBPort { get; set; }
+
+        public int BufferCount { get; set; }
+
+        public override IObservable<IList<Frame>> Generate()
+        {
+            return Observable.Using(
+                () =>
+                {
+                    var ctx = new oni.Context("ft600", 0);
+
+                    // Set read pre-allocation size
+                    ctx.BlockReadSize = 2048;
+                    ctx.BlockWriteSize = 2048;
+
+                    return ctx;
+                },
+                (ctx) => Observable.Create<Frame>(observer =>
+                {
+
+                    ctx.Start(true);
+                    var running = true;
+
+                    var thread = new Thread(() =>
+                    {
+                        while (running)
+                        {
+                            observer.OnNext(ctx.ReadFrame());
+                        }
+                    });
+
+                    thread.Start();
+
+                    return () =>
+                    {
+                        running = false;
+                        if (thread != Thread.CurrentThread) thread.Join();
+                        ctx.Stop();
+                    };
+                })
+                .Buffer(BufferCount)
+                .PublishReconnectable()
+                .RefCount()
+            );
+        } // ctx.Dispose() is called.
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Bonsai.OpenEphys
+[Bonsai](https://bonsai-rx.org/) library for some [
+Open Ephys](https://open-ephys.org) hardware.
+
+## Supported Hardware
+
+- ONI-compatible version of the Open Ephys Acquisition Board
